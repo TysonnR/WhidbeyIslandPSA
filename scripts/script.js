10aeb70001e8be16c4d2718f7f438b8e1f66f7c7
TysonnR <TysonnR@users.noreply.github.com>
2025-01-05 16:37:52 -0800

    Initial commit from W3Schools

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Tyson Ringelstetter
+    File Name: script.js
+    Date: November 11, 2024
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height()); 
+});
